727be7acc188f6bed8662f40e4eb089e41da58b6
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-10-27 19:34:58 +0300

    Create com.java

---
+++ com.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class com {
+    public static void main(String[] args)
+    {
+        float y=1;
+        float x;
+        for (x=-5;x<=5;x++){
+            if (y==0)
+                break;
+            y = 1/x;
+
+
+
+        System.out.println(x+" "+y);}
+    }
+}
